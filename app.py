6efd6813cc2b45d3eb79ab63f2386d567ead0731
amberh <amberh@aweber.com>
2019-08-05 22:45:57 -0400

    add logconfig

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import hashlib
+from logging.config import dictConfig
 import os
 import urllib
 
@@ -13,6 +14,21 @@ import uuid
 import waitress
 
 app = Flask(__name__)
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 app.config.from_mapping({
     "DEBUG": False,
     "PREFERRED_URL_SCHEME": "https"
