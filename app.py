09fd77b5048577bf3863b5b6c519500b63131639
amberh <amberh@aweber.com>
2019-12-22 11:46:28 -0500

    add reconnect logic for psycopg2

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ CREDENTIALS_SECRET = os.environ.get('CREDENTIALS_SECRET', 'secret')
 TRUSTED_ORIGINS = os.environ.get('TRUSTED_ORIGINS', 'localhost 127.0.0.1')
 CORS(app, resources={r"/sponsor": {"origins": TRUSTED_ORIGINS,
                                    "allowed_headers": ["content-type"]}})
-app.conn = psycopg2.connect(os.environ['DATABASE_URL'])  # TODO: reconnect logic
+
 app.secret_key = os.environ['SECRET_KEY']
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -100,6 +100,38 @@ INSERT_CREDENTIALS = ('INSERT INTO credentials'
                       '            (name, credentials, modified_at)'
                       '     VALUES (%s, %s, now())')
 UPDATE_CREDENTIALS = 'UPDATE credentials SET credentials=%s WHERE name=%s'
+
+
+def get_db_conn():
+    max_retries = int(os.environ.get('DB_CONN_MAX_RETRY', 5))
+
+    def _get_conn():
+        conn = psycopg2.connect(os.environ['DATABASE_URL'])
+        conn.set_session(autocommit=True)
+        return conn
+
+    if not getattr(app, 'conn', None):
+        for r in range(1, max_retries + 1):
+            try:
+                setattr(app, 'conn',  _get_conn())
+                return app.conn
+            except psycopg2.OperationalError:
+                app.logger.exception('Failed to create db conn retry:%s', r)
+                if r == max_retries:
+                    raise
+    return app.conn
+
+
+def get_cursor(*args, **kwargs):
+    try:
+        conn = get_db_conn()
+        cur = conn.cursor(*args, **kwargs)
+        return cur
+    except (psycopg2.InterfaceError, psycopg2.OperationalError):
+        app.logger.info('Refreshing db connection')
+        app.conn = None
+        conn = get_db_conn()
+        return conn.cursor(*args, **kwargs)
 
 
 @app.route("/index", methods=['GET'])
@@ -352,7 +384,7 @@ def execute_sql(*sql_dict, raise_error=None, cursor_factory=None):
     error = None
     app.logger.debug('Running query %r', sql_dict)
     try:
-        with app.conn.cursor(cursor_factory=cursor_factory) as cur:
+        with get_cursor(cursor_factory=cursor_factory) as cur:
             for sql in sql_dict:
                 if sql.get('values'):
                     cur.execute(sql['sql'], sql['values'])
@@ -365,14 +397,11 @@ def execute_sql(*sql_dict, raise_error=None, cursor_factory=None):
                 else:
                     pass
             cur.close()
-        app.conn.commit()
     except psycopg2.Error as e:
         error = e
-        app.conn.rollback()
         app.logger.exception('Encountered db error sql: %r', sql_dict)
     except Exception as e:
         error = e
-        app.conn.rollback()
         app.logger.exception('Encountered unknown error sql: %r', sql_dict)
     if raise_error and isinstance(error, raise_error):
         raise error
